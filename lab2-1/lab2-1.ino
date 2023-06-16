bbd661cd3515f8d985b3d9a93d9bd143816af9f9
punpun6699 <191panpan@gmail.com>
2023-06-16 15:14:22 +0700

    update

--- lab2-1/lab2-1.ino
+++ lab2-1/lab2-1.ino
@@ -1,7 +1,7 @@
-int led[]={16,5,4,0};
+int led[]={16,5,4,0,2};
 void setup() {
   // put your setup code here, to run once:
-  for (int i = 0; i<4;i++){
+  for (int i = 0; i<5;i++){
 pinMode(led[i],OUTPUT);}
 digitalWrite(16,0);
 Serial.begin(9600);
@@ -14,4 +14,7 @@ void loop() {
 Serial.println("on");
 delay(1000);
 digitalWrite(led[i],0);}
+digitalWrite(led[4],1);
+delay(1000);
+digitalWrite(led[4],0);
 }
